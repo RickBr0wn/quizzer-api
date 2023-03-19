6ac7164fce06edfdd6ab2b01862a07d37dda71ac
Rick Brown <ricky.brown.00@gmail.com>
2023-03-19 16:34:31 +0000

    adds dummy route

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
-import express, { Application } from 'express'
+import express, { Application, Response, Request } from 'express'
 import questionsRoute from './routes/questions-route'
 
 const app: Application = express()
+
+app.use('/', (res: Response, req: Request) => {
+	res.json({ test: 'rest' })
+})
 
 app.use('/api/questions', questionsRoute)
 
