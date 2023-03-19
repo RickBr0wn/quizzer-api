550ea7d134711b7d1b4cfb76ea3b5e34a8fc32de
Rick Brown <ricky.brown.00@gmail.com>
2023-03-19 18:02:05 +0000

    corrects naming structure

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,4 +1,4 @@
-import getQuestionsByAmount from './getQuestionsByAmount'
+import getQuestionsByAmount from './get-questions-by-amount'
 import healthCheck from './health-check'
 
 export default { healthCheck, getQuestionsByAmount }
