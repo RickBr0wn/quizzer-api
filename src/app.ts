026168ed43abca679721f3f0f60973f475db3ac1
Rick Brown <ricky.brown.00@gmail.com>
2023-03-19 21:56:43 +0000

    oh dear

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,24 @@
 import express, { Application, Response, Request } from 'express'
 import questionsRoute from './routes/questions-route'
+import bodyParser from 'body-parser'
 
 const app: Application = express()
 
-app.use('/', (res: Response, req: Request) => {
-	res.json({ test: 'rest' })
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', (req: Request, res: Response) => {
+	res.json({ message: 'blody hell!' })
 })
 
 app.use('/api/questions', questionsRoute)
+
+app.use((req: Request, res: Response) => {
+	const error = new Error('not found')
+
+	return res.status(400).json({
+		message: error.message
+	})
+})
 
 export default app
